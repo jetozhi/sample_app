f3e56311effbbd1cd7b3b5ea3f12e2177a3beaff
Jetro Tull Roa <jetrotullroa@gmail.com>
2015-06-06 16:50:49 +0800

    Added a Login Page

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,12 @@
+module SessionsHelper
+
+	#Login user
+
+	def log_in(user)
+		session[:user_id] = user_id
+	end
+
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+end
